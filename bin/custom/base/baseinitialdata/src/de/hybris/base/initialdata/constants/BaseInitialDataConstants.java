f6008de07ed3306a2a6b0efa24fd45f8da465be0
Aleksandr Kachura <a.kachura@aimprosoft.com>
2017-08-03 10:48:46 +0300

    init commit

---
+++ bin/custom/base/baseinitialdata/src/de/hybris/base/initialdata/constants/BaseInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2017 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package de.hybris.base.initialdata.constants;
+
+/**
+ * Global class for all BaseInitialData constants.
+ */
+public final class BaseInitialDataConstants extends GeneratedBaseInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "baseinitialdata";
+
+	private BaseInitialDataConstants()
+	{
+		//empty
+	}
+}
